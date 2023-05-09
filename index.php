9815536d5417083b82b9345da155710201dcb500
Michael <michael.vicente1705@gmail.com>
2023-05-09 14:28:18 -0300

    pw

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>STARS WARS</title>
+</head>
+
+<body>
+
+    <video id="background-video" autoplay muted loop>
+        <source src="fundo.mp4" type="video/mp4">
+    </video>
+    <div class="cima">
+
+
+
+        <img src="star.png">
+    </div>
+    <div class="alinhar">
+        <div class="baixo">
+
+            <ul class="nave">
+                <li><a href="personagem.php">Personagens</a></li>
+                <li> <a href="">Filmes</a></li>
+                <li><a href=""> Planetas</a></li>
+                <li><a href="">Especies</a></li>
+                <li><a href="">Veiculos</a></li>
+                <li><a href="">Naves</a></li>
+            </ul>
+
+
+
+
+
+
+
+
+
+        </div>
+    </div>
+
+</body>
+
+</html>
